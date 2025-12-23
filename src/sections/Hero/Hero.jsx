2b2437ee1041a6b31e639902768fe953e6b31124
Evgeniy Zhitnik <jsi.zhitnik@gmail.com>
2025-12-23 15:08:15 +0300

    feat: добавление страницы About
    — модификация секции Hero

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,24 +1,68 @@
 import Results from '@/components/Results';
 import './Hero.scss';
 import { Image } from 'minista';
+import clsx from 'clsx';
 
 export default (props) => {
   const {
     label = '',
   } = props;
 
+  const heroData = {
+    about: [
+      {
+        title: "About",
+        description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+        imgSrc: "./src/assets/images/hero/2.jpg",
+        alt: "A team of people are considering a business project"
+      },
+    ],
+    servisec: [
+      {
+        title: "Servisec",
+        description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+        imgSrc: "./src/assets/images/hero/3.jpg",
+        alt: "Design of a cozy room with a sofa and armchairs"
+      },
+    ],
+    teams: [
+      {
+        title: "Teams",
+        description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+        imgSrc: "./src/assets/images/hero/4.jpg",
+        alt: "Team hands on the table"
+      },
+    ],
+    portfolio: [
+      {
+        title: "Portfolio",
+        description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+        imgSrc: "./src/assets/images/hero/5.jpg",
+        alt: "A cozy room with a desk and a laptop"
+      },
+    ],
+    room: [
+      {
+        title: "Minimalist Room",
+        description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+        imgSrc: "./src/assets/images/hero/6.jpg",
+        alt: "Minimalist room with a sofa and table"
+      },
+    ],
+  };
+
   const getHeroBodyOfTheHomePage = () => {
     return (
       <section className="hero" aria-label="hero-label">
-        <header className="hero__header container">
+        <header className={clsx("hero__header hero__header--home container")}>
           <h1 className="hero__label" id="hero-label">Design your <br /> interor with high <br /> quality.</h1>
           <p className="hero__copyright">
-            <time datetime="2022">2022</time>
+            <time dateTime="2022">2022</time>
             <br />
             all right reserved
           </p>
         </header>
-        <div className="hero__body container">
+        <div className={clsx("hero__body hero__body--home container")}>
           <Results className="hero__results" />
           <Image 
             className="hero__image" 
@@ -30,11 +74,33 @@ export default (props) => {
     );
   };
 
+  const getDefaultPageBody = (label) => {
+    return (
+      <section className={clsx("hero", "container")} aria-label="hero-label">
+        {heroData[label].map((({ title, description, imgSrc, alt }, index) => (
+          <>
+            <header className="hero__header">
+              <h1 className="hero__title" id="hero-label">{title}</h1>
+              <div className="hero__descriprion">{description}</div>
+            </header>
+            <div className="hero__body" key={index}>
+              <Image 
+                className="hero__image" 
+                src={imgSrc}
+                alt={alt}
+              />
+            </div>
+          </>
+        )))}
+      </section>
+    );
+  };
+
   return (
     <div>
       {label === "home"
         ? getHeroBodyOfTheHomePage()
-        : getDefaultPageBody()
+        : getDefaultPageBody(label)
       }
     </div>
   );
